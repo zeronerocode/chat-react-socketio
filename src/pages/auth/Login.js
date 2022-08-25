7ea3cc3b34eceb1eda7952bce2e3b57f95d5de1a
Akmal Fadhilah <fadhilah@mhs.unsyiah.ac.id>
2022-08-25 10:49:10 +0700

    debug url

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -24,12 +24,12 @@ function Login({ setSocket }) {
   const handleLogin = (e) => {
     e.preventDefault();
     axios
-      .post(`${process.env.REACT_APP_API_BACKEND}/v1/users/login`, formLogin)
+      .post(`https://telechatapp.herokuapp.com/v1/users/login`, formLogin)
       .then((res) => {
         const respData = res.data.data;
         localStorage.setItem("token", respData.token);
         localStorage.setItem("refreshToken", respData.refreshToken);
-        const resultSocket = io(`${process.env.REACT_APP_API_BACKEND}`, {
+        const resultSocket = io(`https://telechatapp.herokuapp.com`, {
           query: {
             token: respData.token
           }
